14ff82c3d05aad9bc8f6622fa5df30382bcaee77
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-02-20 08:23:22 -0500

    basic edits.

--- LLM/educ_ai_tion/lib/services/file_service.dart
+++ LLM/educ_ai_tion/lib/services/file_service.dart
@@ -1,4 +1,4 @@
-// File Servic
+// File Service
 // 
 // Manages file operations within the app. This includes uploading, downloading, and storing files. 
 // It is primarily used for handling test templates and educational materials that users wish to work with in the app.
