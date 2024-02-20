fbc3a9841993db2d2e289ac0cb04ac43ca33b721
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-02-20 13:50:15 -0500

    Created a basic screen for file upload and a method to navigate to and from the main screen.

--- LLM/educ_ai_tion/lib/main.dart
+++ LLM/educ_ai_tion/lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'screens/file_upload_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Main Screen'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Builder( // Using Builder to provide a valid context for Navigator. ensures that the context is valid when the ElevatedButton is pressed. Descendant of MaterialApp.
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Welcome to ESS!'),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Using the context provided by Builder.
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FileUploadScreen()),
+                      );
+                    },
+                    child: const Text('I want to upload a file!'),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
