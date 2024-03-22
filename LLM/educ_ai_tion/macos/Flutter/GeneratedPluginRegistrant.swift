da7433571876d8233f4bbb27707e78cabb173394
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-22 12:44:04 -0400

    Added ".env" to assets in Pubspec.

--- LLM/educ_ai_tion/macos/Flutter/GeneratedPluginRegistrant.swift
+++ LLM/educ_ai_tion/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,6 +19,7 @@ import firebase_ml_model_downloader
 import firebase_remote_config
 import firebase_storage
 import path_provider_foundation
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -36,5 +37,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
