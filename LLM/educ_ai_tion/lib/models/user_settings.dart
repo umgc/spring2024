659a3ca9ef292f135cb97ce464a9af29d4061a7e
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-23 09:34:20 -0400

    Deleted unused files.

--- LLM/educ_ai_tion/lib/models/user_settings.dart
+++
@@ -1,4 +0,0 @@
-// User Settings Model
-// 
-// Contains the configuration preferences of the user. This model stores settings such as preferred themes, 
-// notification preferences, and any custom settings related to question generation or app behavior.
